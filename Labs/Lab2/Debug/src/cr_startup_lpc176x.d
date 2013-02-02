7e8c54c873058af2379fd200f0a51bc93651dbb6
danu206 <daniel.ugelstad@my.ndsu.edu>
2013-02-02 00:44:13 -0600

    Attached is my lab2.

---
+++ Labs/Lab2/Debug/src/cr_startup_lpc176x.d
@@ -0,0 +1,5 @@
+src/cr_startup_lpc176x.d src/cr_startup_lpc176x.o: \
+ ../src/cr_startup_lpc176x.c \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
---
+++ Labs/Lab2/Debug/src/main.d
@@ -0,0 +1,16 @@
+src/main.d src/main.o: ../src/main.c \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+
+C:\Workspace\Embedded2\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
